170329398d619445a88b957119f62b1f04b2ec11
NikitOS-RZA <36643375+NikitOS-RZA@users.noreply.github.com>
2018-02-21 18:38:00 +0200

    Update 01-strings-tasks.js

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -55,7 +55,7 @@ function getStringLength(value) {
  *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
  */
 function getStringFromTemplate(firstName, lastName) {
-    throw new Error('Not implemented');
+    return ( ` Hello, ${firstName} ${lastName}` );
 }
 
 /**
@@ -69,7 +69,7 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
 function extractNameFromTemplate(value) {
-    return ( ` Hello, ${firstName} ${lastName}` );
+     throw new Error('Not implemented');
 }
 
 
