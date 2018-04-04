1395b4310aee6049d0de4b73aa7bde58d313ab0f
NikitOS-RZA <36643375+NikitOS-RZA@users.noreply.github.com>
2018-04-04 21:45:38 +0200

    Update 04-arrays-tasks.js

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -23,7 +23,7 @@
  *    [0, 1, 2, 3, 4, 5], 5    => 5
  */
 function findElement(arr, value) {
-   throw new Error('Not implemented');
+   return arr.indexOf(value);
 }
 
 /**
@@ -38,7 +38,11 @@ function findElement(arr, value) {
  *    5 => [ 1, 3, 5, 7, 9 ]
  */
 function generateOdds(len) {
-   throw new Error('Not implemented');
+  var arr = [1];
+	while (arr.length<len){
+		arr.push(arr[arr.length-1]+2)
+	}
+   return arr;
 }
 
 
@@ -54,7 +58,7 @@ function generateOdds(len) {
  *    [] => [] 
  */
 function doubleArray(arr) {
-   throw new Error('Not implemented');
+   return arr.concat(arr);
 }
 
 
